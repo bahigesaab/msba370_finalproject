766bdae8b47d877963558bd221ec72417cc9ecb6
Bahige <bahigesaab8@gmail.com>
2022-05-20 14:08:56 +0300

    align kpis again

--- usa_sales_analytics.py
+++ usa_sales_analytics.py
@@ -132,7 +132,7 @@ def usa_sales_analytics():
     st.markdown(
         """
         <style>
-        .css-1idizjf, .css-zi8otl, .css-16i25t9, .css-18ut3yg{
+        .css-1idizjf, .css-zi8otl, .css-16i25t9, .css-18ut3yg, .css-j5r0tf{
             border: 1px solid;
             text-align: center;
         }
